33ca252bc03b8df62602fa3c14a4b5bb9f5a0880
Falk-Witte <falk.witte15@gmail.com>
2023-01-25 16:56:51 +0100

    logic done, boilerplate code still to be written

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,72 @@
 // Simple cli tool to create web boilerplate code 
+// Could use fs::create_dir function
 
+use clap::Parser;
+use std::fs;
+
+///Cli tool to create web boilerplate code
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args{
+    ///Project name
+    project_name:String,
+}
 
 
 fn main() {
-    
+    let args = Args::parse();
+
+    // create root directory 
+    create_root_dir(&args.project_name);
 }
+
+fn create_root_dir(name: &String) {
+    fs::DirBuilder::new()
+        .create(name)
+        .unwrap_or_else(|e| println!("Error: {}",e));
+
+    //create the html boilerplate code
+    create_html_boil(name);
+
+    // create "static" directory
+    let path_static = name.to_owned() + &String::from("/static");
+    create_static_dir(&path_static);
+
+    //create css boilerplate code
+    create_css_boil(&path_static);
+
+    //create JavaScript boilerplate code
+    create_js_boil(path_static);
+}
+
+fn create_static_dir(path: &String) {
+    fs::DirBuilder::new()
+        .create(path)
+        .unwrap_or_else(|e| println!("Error: {}", e));
+}
+
+fn create_html_boil(root_path: &String){
+    let file_path = root_path.to_owned() + &String::from("/index.html");
+    let html_boil = "am\nto\nlazy\nto\nwrite\nboilerplate\ncode";
+
+    fs::write(file_path, html_boil)
+        .unwrap_or_else(|e| println!("Error: {}", e));
+}
+
+fn create_css_boil(path_static: &String){
+    let file_path = path_static.to_owned() + &String::from("/style.css");
+    let css_boil = "am\nto\nlazy\nto\nwrite\nboilerplate\ncode";
+
+    fs::write(file_path, css_boil)
+        .unwrap_or_else(|e| println!("Error: {}", e));
+}
+
+fn create_js_boil(path_static: String){
+    let file_path = path_static + &String::from("/index.js");
+    let js_boil = "am\nto\nlazy\nto\nwrite\nboilerplate\ncode";
+
+    fs::write(file_path, js_boil)
+        .unwrap_or_else(|e| println!("Error: {}", e));
+}
+
+

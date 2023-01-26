73269a68af2e3baea049972ae535b223b633a119
Falk-Witte <falk.witte15@gmail.com>
2023-01-26 18:24:23 +0100

    this is just a test

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() {
     let args = Opts::parse();
 
     if args.webserver == "y"{
-        println!("Hello, Webserver");
+        println!("Hello, RocketWebserver");
     }
 
     // create root directory 
